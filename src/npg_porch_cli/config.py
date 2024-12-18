82259cca24d632ab50ab7f3cd339f7241009cccf
Kieron Taylor <kt19@sanger.ac.uk>
2024-12-18 16:28:19 +0000

    Replace config-reading code with npg-python-lib npg.conf library

--- src/npg_porch_cli/config.py
+++ src/npg_porch_cli/config.py
@@ -1,23 +1,28 @@
-import configparser
-import json
-import pathlib
 from dataclasses import dataclass
+from os import R_OK, access
+from os.path import isfile
 
-"""Common utility functions for the package."""
-
-DEFAULT_CONF_FILE_TYPE = "ini"
+from npg.conf import IniData
 
 
-def get_config_data(conf_file_path: str, conf_file_section: str = None):
+@dataclass(frozen=True, kw_only=True)
+class PorchClientConfig:
+    """
+    Suggested config file content for interacting with a Porch server instance
+    """
+
+    api_url: str
+    pipeline_name: str
+    pipeline_uri: str
+    pipeline_version: str
+    npg_porch_token: str
+
+
+def get_config_data(
+    conf_file_path: str, conf_file_section: str = "PORCH"
+) -> PorchClientConfig:
     """
     Parses a configuration file and returns its content.
-
-    Allows for two types of configuration files, 'ini' and 'json'. The type of
-    the file is determined from the extension of the file name. In case of no
-    extension an 'ini' type is assumed.
-
-    The content of the file is not cached, so subsequent calls to get data from
-    the same configuration file result in re-reading and re-parsing of the file.
 
     Args:
 
@@ -37,62 +42,11 @@ def get_config_data(conf_file_path: str, conf_file_section: str = None):
       The value corresponding to this key is returned.
     """
 
-    path = pathlib.Path(conf_file_path)
-
-    conf_file_extension = path.suffix
-    if conf_file_extension:
-        conf_file_extension = conf_file_extension[1:]
+    if isfile(conf_file_path) and access(conf_file_path, R_OK):
+        porch_conf = IniData(PorchClientConfig).from_file(
+            conf_file_path, conf_file_section
+        )
     else:
-        conf_file_extension = DEFAULT_CONF_FILE_TYPE
+        raise FileNotFoundError(f"{conf_file_path} is not present or cannot be read")
 
-    if conf_file_extension == DEFAULT_CONF_FILE_TYPE:
-        if not conf_file_section:
-            raise Exception(
-                "'conf_file_section' argument is not given, "
-                "it should be defined for '{DEFAULT_CONF_FILE_TYPE}' "
-                "configuration file."
-            )
-
-        config = configparser.ConfigParser()
-        with open(conf_file_path) as cf:
-            if not cf.readable():
-                raise Exception(f"{path} has a permissions issue")
-
-            config.read_file(cf)
-
-        return {i[0]: i[1] for i in config[conf_file_section].items()}
-
-    elif conf_file_extension == "json":
-        conf: dict = json.load(conf_file_path)
-        if conf_file_section:
-            if isinstance(conf, dict) is False:
-                raise Exception(f"{conf_file_path} does not have sections.")
-            if conf_file_section in conf.keys:
-                conf = conf[conf_file_section]
-            else:
-                raise Exception(
-                    f"{conf_file_path} does not contain {conf_file_section} key"
-                )
-
-        return conf
-
-    else:
-        raise Exception(f"Parsing for '{conf_file_extension}' files is not implemented")
-
-
-@dataclass(frozen=True, kw_only=True)
-class PorchClientConfig:
-    """
-    Suggested config file content for interacting with a Porch server instance
-    """
-
-    api_url: str
-    pipeline_name: str
-    pipeline_uri: str
-    pipeline_version: str
-    npg_porch_token: str
-
-    @classmethod
-    def from_config_file(cls, conf_file_path: str, conf_file_section: str = "PORCH"):
-        conf = get_config_data(conf_file_path, conf_file_section=conf_file_section)
-        return cls(**conf)
+    return porch_conf
